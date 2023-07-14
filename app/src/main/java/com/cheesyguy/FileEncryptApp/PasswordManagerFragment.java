5079ee6cea3fadea93d85e7e02c64d2337def17a
Mohammed Nayaz <mohammednayazan@gmail.com>
2023-07-14 11:03:33 +0530

    home layout design done

---
+++ app/src/main/java/com/cheesyguy/FileEncryptApp/PasswordManagerFragment.java
@@ -0,0 +1,19 @@
+package com.cheesyguy.FileEncryptApp;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class PasswordManagerFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_password_manager, container, false);
+    }
+}
